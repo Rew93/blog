9c113b97d78b29f5b5d81fec6af203bb612c2e9e
Rew93 <bagriyd1m4@gmail.com>
2023-03-11 23:41:09 +0200

    add models and settings

---
+++ events/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+# Create your models here.
+class EventsModel(models.Model):
+    events_image = models.ImageField(upload_to='/events_image')
+    descriptions = models.TextField(max_length=300)
