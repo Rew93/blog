7f0cd5c7534fa287fdce0d38baeae0cc33493d77
Rew93 <bagriyd1m4@gmail.com>
2023-03-12 19:18:41 +0200

    commit Dima

--- events/models.py
+++ events/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class EventsModel(models.Model):
-    events_image = models.ImageField(upload_to='/events_image')
+    events_image = models.ImageField(upload_to='events_image')
     descriptions = models.TextField(max_length=300)
--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class PostModel(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    image = models.ImageField(upload_to='post_image')
